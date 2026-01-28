38a34fee3f59828499738b8055c4fae1fde7ec46
Udemy - Nguyễn Thị Hiền <nguyenhienit94@gmail.com>
2026-01-28 17:13:43 +0700

    chore: delete file

--- tests/authentication/login.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test} from "@playwright/test";
-import { asyncWrapProviders } from "node:async_hooks";
-
-test("Login vào MH home page", async({page}) => {
-    await test.step("Navigate to log", async() => {
-        await page.goto("https://alpha-admin.irismedia.vn/Brandname/BrandnameB2BManagerV2");
-    })
-})
